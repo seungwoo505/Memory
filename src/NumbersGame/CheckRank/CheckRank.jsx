bc19d19227473afb824d0e83ae5873d379586638
seungwoo505 <51819005+seungwoo505@users.noreply.github.com>
2024-02-22 10:57:27 +0900

    Update CheckRank.jsx

--- src/NumbersGame/CheckRank/CheckRank.jsx
+++ src/NumbersGame/CheckRank/CheckRank.jsx
@@ -6,7 +6,7 @@ function CheckRank({navigation, route}){
     const [RankList, setRankList] = useState([]);
     useEffect(() => {
         fetch("https://seungwoo.i234.me/loadRank.php", {
-            method: "GET",
+            method: "POST",
             body: JSON.stringify({
                 Rank: route.params.Rank
             })
